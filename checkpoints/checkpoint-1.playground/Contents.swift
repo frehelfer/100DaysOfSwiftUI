279096c86f98c6f73f7c75f0df85a72939bb3ae5
frehelfer <fre.helfer@gmail.com>
2022-09-15 16:16:39 -0300

    Initial commit

---
+++ checkpoints/checkpoint-1.playground/Contents.swift
@@ -0,0 +1,7 @@
+import Cocoa
+
+let celcius = 35
+
+let fahrenheit = (celcius * 9) / 5 + 32
+
+print(fahrenheit)
---
+++ checkpoints/checkpoint-2.playground/Contents.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+let drumKitArray = ["Ludwig", "Pearl", "Yamaha", "Odery", "DW", "Pearl"]
+
+let drumKitSet = Set(drumKitArray)
+
+print(drumKitArray.count)
+print(drumKitSet.count)
