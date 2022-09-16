445c0b00adc06077a9316e5459792d997b8b7ece
frehelfer <fre.helfer@gmail.com>
2022-09-16 19:49:04 -0300

    checkpoint-5

--- checkpoints/checkpoint-5.playground/Contents.swift
+++ checkpoints/checkpoint-5.playground/Contents.swift
@@ -1,11 +1,4 @@
 import Cocoa
 
-let luckyNumbers = [7, 4, 38, 21, 16, 15,12, 33, 31, 49]
-
-func printLuckyNumbers(_ dataSet: [Int]) {
-    dataSet.filter { !$0.isMultiple(of: 2) }
-        .sorted()
-        .map { print("\($0) is a lucky number") }
-}
-
-printLuckyNumbers(luckyNumbers)
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+    .filter { !$0.isMultiple(of: 2) }.sorted().map { print("\($0) is a lucky number") }
